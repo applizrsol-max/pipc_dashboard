b41f86b0515de1cc6e0123c11cfac4cb6164ee84
Applizr <applizrsol@gmail.com>
2025-11-24 00:40:16 +0530

    changes

--- src/main/java/com/pipc/dashboard/utility/EmailService.java
+++ src/main/java/com/pipc/dashboard/utility/EmailService.java
@@ -1,22 +1,40 @@
 package com.pipc.dashboard.utility;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailService {
 
-	@Autowired
-	private JavaMailSender mailSender;
+	public void sendEmail(String toEmail, String subject, String body) {
 
-	public void sendEmail(String to, String subject, String body) {
+		Email from = new Email("applizrsol@gmail.com"); // your sender email
+		Email to = new Email(toEmail);
 
-		SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setTo(to);
-		msg.setSubject(subject);
-		msg.setText(body);
-		mailSender.send(msg);
+		Content content = new Content("text/plain", body);
+		Mail mail = new Mail(from, subject, to, content);
+
+		SendGrid sg = new SendGrid("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");
+
+		Request request = new Request();
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+
+			Response response = sg.api(request);
+
+			System.out.println("Email Status Code: " + response.getStatusCode());
+			System.out.println("Email Response Body: " + response.getBody());
+			System.out.println("Email Response Headers: " + response.getHeaders());
+
+		} catch (IOException ex) {
+			throw new RuntimeException("Email sending failed: " + ex.getMessage(), ex);
+		}
 	}
 }
